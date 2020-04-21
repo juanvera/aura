7417202a26737491d5db9227361150ac441047d9
juanvera <juan.vera@edosoft.es>
2020-04-21 11:45:29 +0100

    Update to match GDG Gran Canaria Requirements. Modification in firebase config to ensure security.

--- src/components/team/TeamHeader.vue
+++ src/components/team/TeamHeader.vue
@@ -6,7 +6,7 @@
                     <span style="color: #1a73e8;">Our</span> Team </b> 
                 </p>
                 <p class="google-font mt-0 hidden-sm-and-down">Google is known all around the world. Everyone is 'googling', checking on 'maps' and communicating in 'gmail'. For simple users, they are services that just works, but not for us. Developers see much more: APIs, scalability issues, complex technology stacks. And that is what GDG is about.</p>
-                <p class="google-font mt-0 ">Our goal is to organize space to connect the best industry experts with Indian audience to boost development of IT. And Our Core Team is:</p>
+                <p class="google-font mt-0 ">Our goal is to organize space to connect the best industry experts with global audience to boost development of IT. And Our Core Team is:</p>
                 
             </v-col>
         </v-row>
